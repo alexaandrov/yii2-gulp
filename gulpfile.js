38b307cd81ea996810ddfd8e191e53560aeaa761
Grigory Alexandrov <alexaandrov@gmail.com>
2016-09-06 08:59:22 +0300

    Added source directory of the css to watch task

--- gulpfile.js
+++ gulpfile.js
@@ -118,6 +118,7 @@ gulp.task('build', [base.preprocessor, 'js', 'libs', 'imagemin', 'fonts']);
 
 gulp.task('watch', [base.preprocessor, 'libs', 'browser-sync'], function () {
     gulp.watch(path.source + '/' + base.preprocessor + '/**/*.' + base.preprocessor, [base.preprocessor]);
+    gulp.watch(path.source + '/css/**/*.css', [base.preprocessor, browserSync.reload]);
     gulp.watch(path.dest + '/js/**/*.js', browserSync.reload);
     gulp.watch(path.dest + '/css/**/*.css', browserSync.reload);
     gulp.watch(base.part + '/views/**/*.php', browserSync.reload);
